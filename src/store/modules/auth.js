2f55a9dd9bfb2d16123103580dfc4ff9c98fd8ee
designervoid <newluciocheg@gmail.com>
2020-02-29 17:15:25 +0300

    add loader, auth, http helper

---
+++ src/store/modules/auth.js
@@ -0,0 +1,36 @@
+import router from '@/router';
+import {
+  SET_USER
+} from '@/store/mutation-types'
+
+const state = {};
+
+const getters = {};
+
+const actions = {
+  async getToken({
+    commit
+  }, payload) {
+    try {
+      const email = payload.email;
+      const password = payload.password;
+      console.log(email, password);
+      commit(SET_USER, 'test user', {
+        root: true,
+      })
+      router.push('/user-profile')
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
+const mutations = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+};

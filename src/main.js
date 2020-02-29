d777d9b5dffb03d00a92cf4dfa959b14f3257c94
designervoid <newluciocheg@gmail.com>
2020-02-29 12:51:31 +0300

    install buefy, font-awesome, vuelidate

--- src/main.js
+++ src/main.js
@@ -3,8 +3,24 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import Buefy from "buefy";
+import Vuelidate from 'vuelidate'
 
-Vue.config.productionTip = false;
+import "./main.scss";
+
+Vue.config.productionTip = false
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faGlobe);
+Vue.component('vue-fontawesome', FontAwesomeIcon);
+Vue.use(Buefy, {
+  defaultIconComponent: 'vue-fontawesome',
+  defaultIconPack: 'fas',
+});
+Vue.use(Vuelidate)
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+<section>
+  {{ this.$v }}
+  <b-field label="Email" type="is-danger">
+    <b-input  v-model.trim="form.email" @input="setField(event, 'email')">
+    </b-input>
+  </b-field>
+
+  <b-field label="Password" type="is-success">
+    <b-input v-model.trim="form.password" @input="setField(event, 'password')" password-reveal>
+    </b-input>
+  </b-field>
+
+</section>
+</template>
+
+<script>
+import {
+  required,
+  minLength
+} from 'vuelidate/lib/validators'
+
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  validations: {
+    form: {
+      email: {
+        required,
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      }
+    }
+  },
+  methods: {
+    setField(key, value) {
+      this.form[key] = value;
+      this.$v.form[key].$touch()
+    },
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.control.has-icons-left .icon,
+.control.has-icons-right .icon {
+  pointer-events: auto !important;
+}
+
+</style>
